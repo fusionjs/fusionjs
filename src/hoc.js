9ddfd79ed013b33d288486860412e05eb43453c5
Giancarlo Anemone <ganemone@uber.com>
2017-11-03 14:39:13 -0700

    Add support for mapServiceToProps

--- src/hoc.js
+++ src/hoc.js
@@ -2,7 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default {
-  create: name => {
+  create: (name, mapServiceToProps) => {
+    if (!mapServiceToProps) {
+      mapServiceToProps = service => ({[name]: service});
+    }
     return Component => {
       class HOC extends React.Component {
         constructor(props, ctx) {
@@ -10,7 +13,7 @@ export default {
           this.service = ctx[name];
         }
         render() {
-          const props = {...this.props, [name]: this.service};
+          const props = {...this.props, ...mapServiceToProps(this.service)};
           return React.createElement(Component, props);
         }
       }
