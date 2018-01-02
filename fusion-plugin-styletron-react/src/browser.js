1be31ce859022b11ee5bf1b89bb9144ffd134ad0
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-01-02 11:43:57 -0800

    Move client init outside of middleware
    https://github.com/fusionjs/fusion-plugin-styletron-react/pull/20

--- fusion-plugin-styletron-react/src/browser.js
+++ fusion-plugin-styletron-react/src/browser.js
@@ -3,13 +3,16 @@ import React from 'react';
 import Styletron from 'styletron-client';
 import {StyletronProvider} from 'styletron-react';
 
+let styletron;
+
 export default () => (ctx, next) => {
   if (ctx.element) {
-    const styleElements = document.getElementsByClassName(
-      '_styletron_hydrate_'
-    );
-    const styletron = new Styletron(styleElements);
-
+    if (!styletron) {
+      const styleElements = document.getElementsByClassName(
+        '_styletron_hydrate_'
+      );
+      styletron = new Styletron(styleElements);
+    }
     ctx.element = (
       <StyletronProvider styletron={styletron}>{ctx.element}</StyletronProvider>
     );
