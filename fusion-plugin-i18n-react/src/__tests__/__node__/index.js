f605714b8f09c80bdd85f59318d9e0efdf825867
Leo Horie <leohorie@hotmail.com>
2018-01-12 10:36:01 -0800

    Migrate to fusion-core 0.3.0-x (DI API)
    https://github.com/fusionjs/fusion-plugin-i18n-react/pull/20

--- fusion-plugin-i18n-react/src/__tests__/__node__/index.js
+++ fusion-plugin-i18n-react/src/__tests__/__node__/index.js
@@ -4,26 +4,15 @@ import test from 'tape-cup';
 import Plugin from '../../plugin';
 import withI18n from '../../hoc';
 
-function getTranslationsLoader(translations, locale) {
-  return {
-    of() {
-      return {
-        translations,
-        locale,
-      };
-    },
-  };
-}
-
 test('plugin', t => {
   const data = {test: 'hello', interpolated: 'hi ${value}'};
 
-  const TranslationsLoader = getTranslationsLoader(data, 'en-US');
-
   t.equals(typeof Plugin, 'function');
 
-  const I18n = Plugin({TranslationsLoader});
-  const ctx = {headers: {'accept-language': 'en-US'}};
+  const I18n = Plugin({
+    loadTranslations: () => ({translations: data, locale: 'en-US'}),
+  });
+  const ctx = {headers: {'accept-language': 'en-US'}, memoized: new Map()};
   t.equals(typeof I18n.of(ctx).translate, 'function');
   t.end();
 });
