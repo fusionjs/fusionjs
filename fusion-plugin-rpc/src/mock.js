6e407f0b26ed524a4850d3bbb8b854faded7daa2
Alexander Smith <AlexMSmithCA@gmail.com>
2018-01-18 18:41:21 -0800

    Add MIT license header to all source files (#50)
    * Added MIT license header to all source files
    
    * Revised license with minified version

--- fusion-plugin-rpc/src/mock.js
+++ fusion-plugin-rpc/src/mock.js
@@ -1,3 +1,9 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import {withDependencies} from 'fusion-core';
 import MissingHandlerError from './missing-handler-error';
 import {RPCHandlersToken} from './tokens';
