7b7e250d36b69f12c7e3cf31ad2945647ca33b51
Giancarlo Anemone <ganemone@gmail.com>
2018-01-18 16:45:35 -0800

    Pass `ctx` as prop to provider component
    https://github.com/fusionjs/fusion-react/pull/37

--- src/__tests__/hoc.node.js
+++ src/__tests__/hoc.node.js
@@ -13,6 +13,7 @@ tape('hoc', async t => {
   function TestComponent(props) {
     didRender = true;
     t.deepLooseEqual(props.test, testProvides);
+    t.notok(props.ctx, 'does not pass ctx through by default');
     return React.createElement('div', null, 'hello');
   }
   const testPlugin = plugin.create(
@@ -26,6 +27,43 @@ tape('hoc', async t => {
   const ctx = await sim.render('/');
   t.ok(ctx.body.includes('hello'));
   t.ok(didRender);
+  t.end();
+});
+
+tape('hoc with custom BaseComponent', async t => {
+  const withTest = hoc.create('test');
+  const testProvides = {hello: 'world'};
+  let didRender = false;
+  let didUseBase = false;
+  function TestComponent(props) {
+    didRender = true;
+    t.deepLooseEqual(props.test, testProvides);
+    t.notok(props.ctx, 'does not pass ctx through by default');
+    return React.createElement('div', null, 'hello');
+  }
+  const testPlugin = plugin.create(
+    'test',
+    createPlugin({provides: () => testProvides}),
+    class BaseComponent extends React.Component {
+      constructor(props, context) {
+        super(props, context);
+        t.ok(props.ctx, 'passes ctx to base component');
+        t.deepLooseEqual(props.provides, testProvides);
+        didUseBase = true;
+      }
+      render() {
+        return React.Children.only(this.props.children);
+      }
+    }
+  );
+  const element = React.createElement(withTest(TestComponent));
+  const app = new App(element);
+  app.register(testPlugin);
+  const sim = getSimulator(app);
+  const ctx = await sim.render('/');
+  t.ok(ctx.body.includes('hello'));
+  t.ok(didRender);
+  t.ok(didUseBase);
   t.end();
 });
 
