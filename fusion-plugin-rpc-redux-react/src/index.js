c08e4371b2229626f26bb924da8b9ea10b74b439
Alexander Smith <AlexMSmithCA@gmail.com>
2018-01-22 13:57:25 -0800

    Add short MIT license header to all source files (#41)

--- fusion-plugin-rpc-redux-react/src/index.js
+++ fusion-plugin-rpc-redux-react/src/index.js
@@ -1,3 +1,9 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import plugin from './plugin';
 import {ProviderPlugin} from 'fusion-react';
 export {createRPCReducer} from 'fusion-rpc-redux';
