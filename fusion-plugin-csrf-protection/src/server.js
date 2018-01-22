49479d0ca522410f3397ba7435e17e6256652bf1
Alexander Smith <AlexMSmithCA@gmail.com>
2018-01-22 13:58:37 -0800

    Add short MIT license header to all source files (#46)

--- fusion-plugin-csrf-protection/src/server.js
+++ fusion-plugin-csrf-protection/src/server.js
@@ -1,3 +1,9 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import {GenericSessionToken} from 'fusion-tokens';
 import {html, createPlugin} from 'fusion-core';
 import crypto from 'crypto';
