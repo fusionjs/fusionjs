d05d88ac4507f3948755b66145e4ffd429288e13
Alexander Smith <AlexMSmithCA@gmail.com>
2018-01-22 13:56:53 -0800

    Add short MIT license header to all source files (#49)

--- fusion-plugin-i18n-react/src/index.js
+++ fusion-plugin-i18n-react/src/index.js
@@ -1,3 +1,9 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import I18n from './plugin';
 
 export default I18n;
