1a651012235011a14195807fc2dd61692e7406a6
Giancarlo Anemone <ganemone@gmail.com>
2018-01-29 12:31:09 -0800

    Fix tree shaking (#55)
    https://github.com/fusionjs/fusion-plugin-error-handling/pull/55

--- fusion-plugin-error-handling/src/client.js
+++ fusion-plugin-error-handling/src/client.js
@@ -13,39 +13,40 @@ export const ErrorHandlingEmitterToken = createToken(
   'ErrorHandlingEmitterToken'
 );
 
-export default createPlugin({
-  deps: {emit: ErrorHandlingEmitterToken},
-  provides: ({emit}) => {
-    const _emit =
-      (typeof emit === 'function' && emit) ||
-      ((e, src) => {
-        if (window.onerror) window.onerror(e.message, src, null, null, e);
-      });
-    for (const key in window) {
-      if (
-        key.match(/webkit/) == null && // stop deprecation warnings
-        window[key] &&
-        window[key].prototype &&
-        window[key].prototype.addEventListener
-      ) {
-        const proto = window[key].prototype;
-        const old = proto.addEventListener;
-        proto.addEventListener = function(type, fn, ...rest) {
-          const cb = function(...args) {
-            try {
-              return fn.apply(this, args);
-            } catch (e) {
-              // get exception stack frames from our own code rather than potentially from 3rd party CDN code to get around CORS issues
-              _emit(e, 'async-event');
-            }
+export default __BROWSER__ &&
+  createPlugin({
+    deps: {emit: ErrorHandlingEmitterToken},
+    provides: ({emit}) => {
+      const _emit =
+        (typeof emit === 'function' && emit) ||
+        ((e, src) => {
+          if (window.onerror) window.onerror(e.message, src, null, null, e);
+        });
+      for (const key in window) {
+        if (
+          key.match(/webkit/) == null && // stop deprecation warnings
+          window[key] &&
+          window[key].prototype &&
+          window[key].prototype.addEventListener
+        ) {
+          const proto = window[key].prototype;
+          const old = proto.addEventListener;
+          proto.addEventListener = function(type, fn, ...rest) {
+            const cb = function(...args) {
+              try {
+                return fn.apply(this, args);
+              } catch (e) {
+                // get exception stack frames from our own code rather than potentially from 3rd party CDN code to get around CORS issues
+                _emit(e, 'async-event');
+              }
+            };
+            return old.call(this, type, cb, ...rest);
           };
-          return old.call(this, type, cb, ...rest);
-        };
+        }
       }
-    }
-    window.addEventListener('unhandledrejection', e => {
-      e.preventDefault();
-      _emit(e.reason instanceof Error ? e.reason : new Error(e.reason));
-    });
-  },
-});
+      window.addEventListener('unhandledrejection', e => {
+        e.preventDefault();
+        _emit(e.reason instanceof Error ? e.reason : new Error(e.reason));
+      });
+    },
+  });
