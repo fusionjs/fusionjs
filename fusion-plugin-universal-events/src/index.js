3d7baa33f193df319f6dfdcbc0572824a4ce31d2
Kevin Grandon <kevingrandon@yahoo.com>
2018-02-12 07:33:16 -0800

    Implement minimial flow support
    https://github.com/fusionjs/fusion-plugin-universal-events/pull/87

--- fusion-plugin-universal-events/src/index.js
+++ fusion-plugin-universal-events/src/index.js
@@ -4,6 +4,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// @flow
 import {createToken} from 'fusion-core';
 import ServerEvents from './server.js';
 import BrowserEvents from './browser.js';
