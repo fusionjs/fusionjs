910d2ead1f4ded0ea9520f029281233d584b5f50
Kevin Grandon <kevingrandon@yahoo.com>
2018-02-12 10:16:05 -0800

    Implement minimial flow support (#82)
    While this won't provide full coverage, it will allow folks to use flow without ignoring fusion imports.

---
+++ fusion-plugin-error-handling/flow-typed/globals.js
@@ -0,0 +1,4 @@
+// @flow
+
+declare var __NODE__: boolean;
+declare var __BROWSER__: boolean;
--- fusion-plugin-error-handling/src/index.js
+++ fusion-plugin-error-handling/src/index.js
@@ -4,6 +4,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// @flow
 import serverPlugin from './server';
 import clientPlugin from './client';
 
