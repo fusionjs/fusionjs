2b797144b57881dd1ff2ae7792690002ad0ad6cb
Alexander Smith <AlexMSmithCA@gmail.com>
2018-04-18 16:57:49 -0700

    Fix .flowconfig to not overwrite FlowFixMe
    https://github.com/fusionjs/fusion-plugin-i18n/pull/119

---
+++ fusion-plugin-i18n/flow-typed/locale.js
@@ -0,0 +1,35 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+declare module 'locale' {
+  declare class Locale {
+    constructor(str: string): Locale;
+    normalized: string;
+
+    serialize: Function;
+    toString: Function;
+    toJSON: Function;
+  }
+
+  declare class Locales {
+    constructor(str: string[], def?: string): Locales;
+
+    length: number;
+    _index: any;
+    index: Function;
+
+    best: Function;
+
+    sort: Function;
+    push: Function;
+
+    serialize: Function;
+    toString: Function;
+    toJSON: Function;
+  }
+}
