8225b75c38904c17ebfa95f08820f3f0743f93a4
Giancarlo Anemone <ganemone@gmail.com>
2018-04-24 16:56:05 -0700

    Fix export of Helmet
    https://github.com/fusionjs/fusion-plugin-react-helmet-async/pull/19

--- fusion-plugin-react-helmet-async/src/index.js
+++ fusion-plugin-react-helmet-async/src/index.js
@@ -5,9 +5,10 @@
  */
 
 // @flow
+import Helmet from 'react-helmet-async';
 import serverPlugin from './server';
 import clientPlugin from './browser';
 
 declare var __NODE__: Boolean;
 export default (__NODE__ ? serverPlugin : clientPlugin);
-export {Helmet} from 'react-helmet-async';
+export {Helmet};
