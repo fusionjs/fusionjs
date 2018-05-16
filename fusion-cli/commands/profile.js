dff48dce8739832466d743fa166c45c400dff56a
Leo Horie <leohorie@hotmail.com>
2018-05-16 16:28:13 -0700

    Fix profiler (#363)

--- fusion-cli/commands/profile.js
+++ fusion-cli/commands/profile.js
@@ -1,18 +1,10 @@
 /* eslint-env node */
-const path = require('path');
-const fs = require('fs');
-const profile = require('../lib/profiler');
-const launchSourceMapExplorer = require('../launch-source-map-explorer.js');
+const {Compiler} = require('../build/compiler');
+const analyzer = require('bundle-analyzer');
 
-exports.command = 'profile [--dir] [--environment] [--watch] [--file-count]';
+exports.command = 'profile [--dir] [--environment] [--port]';
 exports.desc = 'Profile your application';
 exports.builder = {
-  // TODO(#18): support fusion profile --watch
-  // 'skip-build': {
-  //   type: 'boolean',
-  //   default: false,
-  //   describe: 'Use existing built assets',
-  // },
   dir: {
     type: 'string',
     default: '.',
@@ -23,55 +15,26 @@ exports.builder = {
     default: 'production',
     describe: 'Either `production` or `development`',
   },
-  watch: {
-    type: 'boolean',
-    default: false,
-    describe: 'After profiling, launch source-map-explorer with file watch',
-  },
-  'file-count': {
+  port: {
     type: 'number',
-    default: 20,
-    describe:
-      'The number of file sizes to output, sorted largest to smallest (-1 for all files)',
+    default: '4000',
+    describe: 'Port for the bundle analyzer server',
   },
 };
-exports.run = async function profileHandler({
-  dir = '.',
-  environment,
-  fileCount,
-  watch,
-}) {
-  const appBase = path.resolve(process.cwd(), dir);
-  const genFileBase = path.join(appBase, '.fusion');
-  const clientFileBase = path.join(genFileBase, `dist/${environment}/client`);
-  const statsFile = path.join(genFileBase, 'stats.json');
-  if (!fs.existsSync(statsFile)) {
-    throw new Error(
-      'Missing stats file - Please run a production build before profiling your application'
-    );
-  }
-
-  const stats = require(statsFile);
-  const clientEntry = stats.children.find(childStats => {
-    return childStats.name === 'client';
+exports.run = async function profileHandler({dir = '.', port, environment}) {
+  const compiler = new Compiler({envs: [environment], dir, watch: true});
+  const server = analyzer.start({
+    dir: `${dir}/.fusion/dist/${environment}/client`,
+    port,
   });
-
-  if (!clientEntry) {
-    throw new Error('Stats file missing client entry');
-  }
-
-  const {chunks} = clientEntry;
-  const jsMapPairs = chunks.map(chunk => {
-    const sourceFile = path.join(clientFileBase, chunk.files[0]);
-    const mapFile = path.join(clientFileBase, chunk.files[1]);
-    return {
-      sourceFile,
-      mapFile,
-    };
+  const watcher = compiler.start(() => {
+    server.update();
   });
-
-  profile({dir: dir, pairs: jsMapPairs, length: fileCount});
-  if (watch) {
-    launchSourceMapExplorer({dir: dir});
-  }
+  return {
+    compiler,
+    stop() {
+      watcher.close();
+      server.close();
+    },
+  };
 };
