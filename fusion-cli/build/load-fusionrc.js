9b3cc14c1bf7a0d4c91f001851ed7412b1a374e7
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-05-18 16:41:10 -0700

    Ensure .babelrc notice is only logged once
    https://github.com/fusionjs/fusion-cli/pull/362

--- fusion-cli/build/load-fusionrc.js
+++ fusion-cli/build/load-fusionrc.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 
+let loggedNotice = false;
+
 module.exports = function validateConfig(dir) {
   const configPath = path.join(dir, '.fusionrc.js');
   let config;
@@ -13,14 +15,17 @@ module.exports = function validateConfig(dir) {
     if (!isValid(config)) {
       throw new Error('.fusionrc.js is invalid');
     }
-    console.log(chalk.dim('Using custom Babel config from .fusionrc.js'));
-    console.warn(
-      chalk.yellow(
-        'Warning: custom Babel config is an',
-        chalk.bold.underline('unstable API'),
-        'and may be not be supported in future releases. Use at your own risk.'
-      )
-    );
+    if (!loggedNotice) {
+      console.log(chalk.dim('Using custom Babel config from .fusionrc.js'));
+      console.warn(
+        chalk.yellow(
+          'Warning: custom Babel config is an',
+          chalk.bold.underline('unstable API'),
+          'and may be not be supported in future releases. Use at your own risk.'
+        )
+      );
+      loggedNotice = true;
+    }
   } else {
     config = {};
   }
