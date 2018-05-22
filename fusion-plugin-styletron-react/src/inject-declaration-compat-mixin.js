b9adc62d7be98d95e196a5bfa731d67fd7c4707f
Kevin Grandon <kevingrandon@yahoo.com>
2018-05-22 10:58:25 -0700

    Add flow declarations to files due to new linter rule
    https://github.com/fusionjs/fusion-plugin-styletron-react/pull/117

--- fusion-plugin-styletron-react/src/inject-declaration-compat-mixin.js
+++ fusion-plugin-styletron-react/src/inject-declaration-compat-mixin.js
@@ -1,5 +1,15 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+// $FlowFixMe
 export const injectDeclarationCompatMixin = Base =>
   class extends Base {
+    // $FlowFixMe
     injectDeclaration({prop, val, media, pseudo}) {
       let style = {[prop]: val};
       if (pseudo) {
--- fusion-plugin-styletron-react/src/legacy-provider.js
+++ fusion-plugin-styletron-react/src/legacy-provider.js
@@ -1,13 +1,24 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
 import React from 'react';
 
 /**
  * Provides styletron instance via old context API
  */
 
-class LegacyStyletronProvider extends React.Component {
+class LegacyStyletronProvider extends React.Component<*, *> {
+  styletron: any;
+
   getChildContext() {
     return {styletron: this.styletron};
   }
+  // $FlowFixMe
   constructor(props, context) {
     super(props, context);
     this.styletron = props.value;
