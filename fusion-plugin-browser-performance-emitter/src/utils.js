9e2e7f69364858b96439d95bef2093e4b8ced248
Kevin Grandon <kevingrandon@yahoo.com>
2018-05-25 14:51:21 -0700

    Add missing annotation and greenkeep (#106)

--- fusion-plugin-browser-performance-emitter/src/utils.js
+++ fusion-plugin-browser-performance-emitter/src/utils.js
@@ -1,4 +1,14 @@
-export function buildPaintTimesObject(paintTimes) {
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+type paintTimesType = Array<{[string]: string}>;
+
+export function buildPaintTimesObject(paintTimes: paintTimesType) {
   return {
     firstPaint: getTimeFromMarks(paintTimes, 'first-paint'),
     firstContentfulPaint: getTimeFromMarks(
@@ -8,7 +18,7 @@ export function buildPaintTimesObject(paintTimes) {
   };
 }
 
-export function getTimeFromMarks(marks, name) {
+export function getTimeFromMarks(marks: paintTimesType, name: string) {
   const thisMark = marks.find(mark => mark.name === name);
   return thisMark && thisMark.startTime;
 }
