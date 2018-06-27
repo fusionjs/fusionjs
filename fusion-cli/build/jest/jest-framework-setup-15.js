35b944d144f971623d08ee0944a83c2d345aa445
Kevin Grandon <kevingrandon@yahoo.com>
2018-06-27 11:28:42 -0700

    Remove enzyme-adapter-react-15 from deps (#403)

--- fusion-cli/build/jest/jest-framework-setup-15.js
+++ fusion-cli/build/jest/jest-framework-setup-15.js
@@ -9,7 +9,13 @@
 /* eslint-env node */
 
 import {configure} from 'enzyme';
+// We do not list enzyme-adapter-react-15 in our dependencies as this breaks peerDeps
+// for all Fusion.js apps using React 16. Instead we allow apps to manually install
+// this package for React 15 support.
+/* eslint-disable import/no-unresolved */
+// $FlowFixMe
 import Adapter from 'enzyme-adapter-react-15';
+/* eslint-enable import/no-unresolved */
 
 // Setup Enzyme for all Jest tests
 configure({adapter: new Adapter()});
