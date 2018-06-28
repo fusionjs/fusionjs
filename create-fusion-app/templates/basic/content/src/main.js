07698300b7f97524bdc05a146901329381b14208
Austin Green <austingreenkansas@gmail.com>
2018-06-28 14:18:38 -0700

    Remove fusion-plugin-universal-events registration (#15)

--- create-fusion-app/templates/basic/content/src/main.js
+++ create-fusion-app/templates/basic/content/src/main.js
@@ -1,9 +1,6 @@
 // @flow
 import App from 'fusion-react';
 import Router from 'fusion-plugin-react-router';
-import UniversalEvents, {
-  UniversalEventsToken,
-} from 'fusion-plugin-universal-events';
 import Styletron from 'fusion-plugin-styletron-react';
 
 import root from './root.js';
@@ -12,6 +9,5 @@ export default () => {
   const app = new App(root);
   app.register(Styletron);
   app.register(Router);
-  app.register(UniversalEventsToken, UniversalEvents);
   return app;
 };
