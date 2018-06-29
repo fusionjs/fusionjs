cc0f8b8579e16465f56a50e30d2daf35694c7e9f
angus croll <anguscroll@gmail.com>
2018-06-29 11:21:27 -0700

    Add render benchmark to performance API (#246)
    * add render benchmark to performance API

--- fusion-core/src/plugins/ssr.js
+++ fusion-core/src/plugins/ssr.js
@@ -120,6 +120,7 @@ function getCoreGlobals(ctx) {
 
   return [
     `<script nonce="${nonce}">`,
+    `window.performance && window.performance.mark && window.performance.mark('firstRenderStart');`,
     `__ROUTE_PREFIX__ = ${JSON.stringify(ctx.prefix)};`, // consumed by ./client
     `__WEBPACK_PUBLIC_PATH__ = ${JSON.stringify(webpackPublicPath)};`, // consumed by fusion-clientries/client-entry
     manifest,
