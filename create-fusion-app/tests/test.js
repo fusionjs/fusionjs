c30985886707d3442b30de9341c8e09b7a76fee9
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-12-17 13:50:36 -0800

    Upgrade fusion-cli, fusion-core, and styletron-react
    https://github.com/fusionjs/create-fusion-app/pull/59

--- create-fusion-app/tests/test.js
+++ create-fusion-app/tests/test.js
@@ -4,6 +4,7 @@
 const {promisify} = require('util');
 const exec = promisify(require('child_process').exec);
 const {startServer} = require('../test-utils/test-utils.js');
+const puppeteer = require('puppeteer');
 
 function log(execOutput) {
   // eslint-disable-next-line no-console
@@ -20,9 +21,16 @@ test('scaffolded app tests pass', async () => {
   log(await exec(`yarn build`, options));
 
   // Spin up server and validate SSR response
-  const {initialResponse, proc} = await startServer();
+  const {port, proc} = await startServer();
 
-  expect(initialResponse).toContain('Fusion.js');
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${port}`);
+  const response = await page.content();
+  await browser.close();
+  expect(response).toContain('Fusion.js');
 
   proc.kill();
 }, 300000);
