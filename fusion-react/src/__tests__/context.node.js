4c7b740b16e9b01e3d2b95fde8625379ec296e4d
Mickey Burks <mickey@burks.online>
2019-04-10 09:29:03 -0700

    Add useService hook and modern Context
    https://github.com/fusionjs/fusion-react/pull/242
    
    Co-authored-by: Mickey Burks <mburks@uber.com>

---
+++ fusion-react/src/__tests__/context.node.js
@@ -0,0 +1,109 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import test from 'tape-cup';
+import * as React from 'react';
+import {createToken, createPlugin} from 'fusion-core';
+import {getSimulator} from 'fusion-test-utils';
+import App from '../index';
+import {
+  FusionContext,
+  serviceContextPlugin,
+  ServiceConsumer,
+  useService,
+} from '../context.js';
+
+test('useService hook', async t => {
+  const TestToken = createToken('test');
+  const TestPlugin = createPlugin({provides: () => 3});
+  let didRender = false;
+  function TestComponent() {
+    const provides = useService(TestToken);
+    const ctx = React.useContext(FusionContext);
+    didRender = true;
+    t.equal(provides, 3, 'gets registered service');
+    t.equal(ctx.request.url, '/', 'gets Fusion context');
+    return React.createElement('div', null, 'hello');
+  }
+  const element = React.createElement(TestComponent);
+  const app = new App(element);
+  app.register(TestToken, TestPlugin);
+  app.register(serviceContextPlugin(app));
+  const sim = getSimulator(app);
+  const ctx = await sim.render('/');
+  t.ok(typeof ctx.body === 'string' && ctx.body.includes('hello'), 'renders');
+  t.ok(didRender);
+  t.end();
+});
+
+test('context error', async t => {
+  let didRender = false;
+  function TestComponent() {
+    const TestToken = createToken('test');
+    useService(TestToken);
+    didRender = true;
+    return React.createElement('div', null, 'hello');
+  }
+  const element = React.createElement(TestComponent);
+  const app = new App(element);
+  app.register(serviceContextPlugin(app));
+  const sim = getSimulator(app);
+  try {
+    await sim.render('/');
+  } catch (e) {
+    t.ok(
+      /Token .* not registered/.test(e.message),
+      'throws when token not registered'
+    );
+  }
+  t.notOk(didRender);
+  t.end();
+});
+
+test('context error with optional token', async t => {
+  let didRender = false;
+  function TestComponent() {
+    const TestToken = createToken('test');
+    useService(TestToken.optional);
+    didRender = true;
+    return React.createElement('div', null, 'hello');
+  }
+  const element = React.createElement(TestComponent);
+  const app = new App(element);
+  app.register(serviceContextPlugin(app));
+  const sim = getSimulator(app);
+  await sim.render('/');
+  t.ok(didRender);
+  t.end();
+});
+
+test('context consumer component', async t => {
+  const TestToken = createToken('test');
+  const TestPlugin = createPlugin({provides: () => 3});
+  let didRender = false;
+  function TestComponent() {
+    return React.createElement(
+      ServiceConsumer,
+      {token: TestToken},
+      provides => {
+        didRender = true;
+        t.equal(provides, 3, 'gets registered service');
+        return React.createElement('div', null, 'hello');
+      }
+    );
+  }
+  const element = React.createElement(TestComponent);
+  const app = new App(element);
+  app.register(TestToken, TestPlugin);
+  app.register(serviceContextPlugin(app));
+  const sim = getSimulator(app);
+  const ctx = await sim.render('/');
+  t.ok(typeof ctx.body === 'string' && ctx.body.includes('hello'), 'renders');
+  t.ok(didRender);
+  t.end();
+});
---
+++ fusion-react/src/context.js
@@ -0,0 +1,66 @@
+// @flow
+import * as React from 'react';
+import {createPlugin} from 'fusion-core';
+import type FusionApp, {FusionPlugin, Middleware} from 'fusion-core';
+import type {Element} from 'react';
+
+export const FusionContext = React.createContext<any>({});
+export const ServiceContext = React.createContext<any>(() => {});
+
+type ReturnsType<T> = () => T;
+
+export function useService<TService>(token: ReturnsType<TService>): TService {
+  const getService: (ReturnsType<TService>) => TService = React.useContext(
+    ServiceContext
+  );
+  const provides = getService(token);
+  return provides;
+}
+
+type ServiceConsumerProps<TService> = {
+  token: ReturnsType<TService>,
+  children: TService => Element<any>,
+};
+
+export function ServiceConsumer<TService>({
+  token,
+  children,
+}: ServiceConsumerProps<TService>) {
+  return (
+    <ServiceContext.Consumer>
+      {(getService: (ReturnsType<TService>) => TService) => {
+        const provides = getService(token);
+        return children(provides);
+      }}
+    </ServiceContext.Consumer>
+  );
+}
+
+export function serviceContextPlugin(app: FusionApp): FusionPlugin<void, void> {
+  function getService(token) {
+    const provides = app.getService(token);
+    const isRequiredToken = Boolean(token.optional);
+    if (typeof provides === 'undefined' && isRequiredToken) {
+      throw new Error(
+        `Token ${
+          token.name
+        } not registered or registered plugin does not provide a service. To use an optional plugin, use \`Token.optional\`.`
+      );
+    }
+    return provides;
+  }
+  return createPlugin({
+    middleware(): Middleware {
+      return (ctx, next) => {
+        ctx.element = ctx.element && (
+          <FusionContext.Provider value={ctx}>
+            <ServiceContext.Provider value={getService}>
+              {ctx.element}
+            </ServiceContext.Provider>
+          </FusionContext.Provider>
+        );
+        return next();
+      };
+    },
+  });
+}
