253cd31fa29950fb9c5c434a330969828ce42daf
Mickey Burks <mburks@uber.com>
2019-05-16 10:48:19 -0700

    Remove babel polyfill, use core-js and regenerator-runtime directly
    https://github.com/fusionjs/fusionjs/pull/390

--- fusion-cli/entries/client-public-path.js
+++ fusion-cli/entries/client-public-path.js
@@ -4,7 +4,8 @@ declare var __webpack_public_path__: string;
 declare var __webpack_nonce__: string;
 */
 
-import '@babel/polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 // eslint-disable-next-line
 __webpack_public_path__ = window.__FUSION_ASSET_PATH__;
