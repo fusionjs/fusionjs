f37fc7c7503d87cea0d40eee56832bf4ced4fb8e
Mickey Burks <mburks@uber.com>
2019-05-17 13:58:47 -0700

    Remove ProviderPlugin override

--- fusion-plugin-rpc-redux-react/src/plugin.js
+++ fusion-plugin-rpc-redux-react/src/plugin.js
@@ -6,39 +6,15 @@
  * @flow
  */
 
-import * as React from 'react';
-import PropTypes from 'prop-types';
-
 import {ProviderPlugin} from 'fusion-react';
 import rpc, {mock as RPCMock} from 'fusion-plugin-rpc';
 import type {RPCType, RPCDepsType} from 'fusion-plugin-rpc';
 
-class RPCProvider extends React.Component<*> {
-  rpc: typeof rpc;
-
-  constructor(props, context) {
-    super(props, context);
-    this.rpc = props.provides.from(props.ctx);
-  }
-  getChildContext() {
-    return {rpc: this.rpc};
-  }
-  render() {
-    return React.Children.only(this.props.children);
-  }
-}
-
-RPCProvider.childContextTypes = {
-  rpc: PropTypes.object.isRequired,
-};
-
 export default ProviderPlugin.create<RPCDepsType, RPCType>(
   'rpc',
   rpc,
-  RPCProvider
 );
 export const mock = ProviderPlugin.create<RPCDepsType, RPCType>(
   'rpc',
   RPCMock,
-  RPCProvider
 );
