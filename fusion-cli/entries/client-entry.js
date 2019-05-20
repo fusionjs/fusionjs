cbe82c321f1af9dee05596bb29501f1f224d3f13
Mickey Burks <mburks@uber.com>
2019-05-20 13:37:42 -0700

    Add error message if App init doesn't return the App instance
    https://github.com/fusionjs/fusionjs/pull/395

--- fusion-cli/entries/client-entry.js
+++ fusion-cli/entries/client-entry.js
@@ -9,13 +9,16 @@
 /* eslint-env browser */
 /* global module */
 
-import {createPlugin, RoutePrefixToken} from 'fusion-core';
+import BaseApp, {createPlugin, RoutePrefixToken} from 'fusion-core';
 
 function reload() {
   // $FlowFixMe
   const main = require('__FUSION_ENTRY_PATH__'); // eslint-disable-line
   const initialize = main.default || main;
   Promise.resolve(initialize()).then(app => {
+    if (!(app instanceof BaseApp)) {
+      throw new Error('Application entry point did not return an App');
+    }
     if (window.__ROUTE_PREFIX__) {
       // No-op plugin so token can be registered without any consumers
       // Should not be needed when route prefixing is refactored into a separate plugin
