92256e4039a9df462e63972cb06e9a4a94f9cf18
Mickey Burks <mburks@uber.com>
2019-05-22 13:48:25 -0700

    Fix lint

--- fusion-plugin-rpc-redux-react/test/fixture/src/root.js
+++ fusion-plugin-rpc-redux-react/test/fixture/src/root.js
@@ -1,4 +1,11 @@
-// @flow
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @noflow
+ */
+
 import React from 'react';
 import {compose} from 'redux';
 import {connect} from 'react-redux';
