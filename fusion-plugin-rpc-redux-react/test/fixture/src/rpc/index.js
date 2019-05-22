473269ee51151665eec006189c6db8fabf9c0169
Mickey Burks <mburks@uber.com>
2019-05-22 13:48:25 -0700

    Fix lint

--- fusion-plugin-rpc-redux-react/test/fixture/src/rpc/index.js
+++ fusion-plugin-rpc-redux-react/test/fixture/src/rpc/index.js
@@ -1,4 +1,11 @@
-// @flow
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @noflow
+ */
+
 export default {
   async getUser() {
     return {id: 123, name: 'bosco'};
