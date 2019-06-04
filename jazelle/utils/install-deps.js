1023176a8dfd6fce8ac58b29155f58f6da03b1e0
Leo Horie <lhorie@uber.com>
2019-06-04 23:14:07 +0000

    fix symlinks for transitive deps
    meta:sha:b8a3798e7ee876d7a662f2b1f6a4624169a96f94

--- jazelle/utils/install-deps.js
+++ jazelle/utils/install-deps.js
@@ -1,7 +1,7 @@
 const lockfile = require('@yarnpkg/lockfile');
 const {dirname} = require('path');
 const {createHash} = require('crypto');
-const {ls, exists, exec, spawn, read, write} = require('./node-helpers.js');
+const {exists, exec, spawn, read, write} = require('./node-helpers.js');
 const {node, yarn} = require('./binary-paths.js');
 
 const installDeps = async (root, deps, hooks = {}) => {
@@ -13,9 +13,9 @@ const installDeps = async (root, deps, hooks = {}) => {
 
   const bin = `${root}/third_party/jazelle/temp`;
   const cache = await getInstallationCache(bin);
-  if (!cache.isCached()) {
+  const isCached = cache.isCached();
+  if (!isCached) {
     await exec(`${node} ${yarn} install --frozen-lockfile --offline --non-interactive --ignore-optional`, {cwd: bin});
-    await cache.save();
   }
 
   const cacheDir = `${bin}/node_modules`;
@@ -24,9 +24,13 @@ const installDeps = async (root, deps, hooks = {}) => {
   await Promise.all(
     deps.map(async dep => {
       const {object} = lockfile.parse(await read(`${dep.dir}/yarn.lock`, 'utf8'));
+      let hoisted = {};
       await spawn('mkdir', ['-p', 'node_modules'], {cwd: dep.dir});
-      await populate(cacheDir, deps, dep, 'dependencies', object);
-      await populate(cacheDir, deps, dep, 'devDependencies', object);
+      hoisted = await populate(cacheDir, deps, dep, 'dependencies', object, hoisted);
+      hoisted = await populate(cacheDir, deps, dep, 'devDependencies', object, hoisted);
+      if (!isCached) {
+        await populateTransitive(root, dep, hoisted);
+      }
     })
   );
 
@@ -34,7 +38,12 @@ const installDeps = async (root, deps, hooks = {}) => {
   if (hooks.postinstall) {
     await exec(`PATH=${dirname(node)}\\:$PATH ${hooks.postinstall}`, {stdio: 'inherit'});
   }
+
+  if (!isCached) {
+    await cache.save();
+  }
 }
+
 async function getInstallationCache(bin) {
   let key = await read(`${bin}/cache-key.txt`, 'utf8').catch(() => '');
   const lock = await read(`${bin}/yarn.lock`, 'utf8');
@@ -49,6 +58,7 @@ async function getInstallationCache(bin) {
     },
   }
 }
+
 async function extract(downloadDir, cacheDir) {
   const tars = (await exec(`find . -type f -name "*.tgz"`, {cwd: downloadDir})).trim().split('\n').sort();
   await Promise.all(
@@ -61,12 +71,13 @@ async function extract(downloadDir, cacheDir) {
     })
   )
 }
-async function populate(cacheDir, deps, dep, type, lockfileObject) {
+
+async function populate(cacheDir, deps, dep, type, lockfileObject, hoisted) {
   await Promise.all(
     Object.keys(dep.meta[type] || {}).map(async name => {
       const versionRange = dep.meta[type][name];
       const item = lockfileObject[`${name}@${versionRange}`] || deps.map(d => d.meta).find(meta => meta.name === name);
-      if (item) {
+      if (item && !hoisted[name]) {
         const aliased = versionRange.startsWith('npm:') ? versionRange.match(/npm:(.[^@]*)/)[1] : name;
         const {version} = item;
         const [ns, basename] = name.includes('@') ? name.split('/') : ['.', name];
@@ -90,9 +101,31 @@ async function populate(cacheDir, deps, dep, type, lockfileObject) {
             await spawn('ln', ['-s', `${depPath}/${commands[cmd]}`, cmd], {cwd: binFolder});
           })
         )
+
+        hoisted[name] = item;
       }
     })
   );
+  return hoisted;
+}
+
+async function populateTransitive(root, dep, hoisted) {
+  const cacheDir = `${root}/third_party/jazelle/temp/node_modules`;
+  const dir = `${dep.dir}/node_modules`;
+  const deps = (await exec('find . -name package.json -type f', {cwd: cacheDir}))
+    .trim()
+    .split('\n')
+    .map(file => dirname(file).slice(2))
+    .filter(file => file.match(/^[a-z0-9-_$]+$|^@[a-z0-9-_$]+\/[a-z0-9-_$]+$/))
+    .filter(dep => !(dep in hoisted));
+  for (let i = 0, size = 300; i < deps.length; i += size) {
+    await Promise.all(
+      deps.slice(i, i + size).map(async dep => {
+        const prep = dep.includes('/') ? `mkdir -p ${dirname(dep)} && ` : '';
+        await exec(`${prep} rm -rf ${dep} && ln -s ${cacheDir}/${dep} ${dep}`, {cwd: dir});
+      })
+    )
+  }
 }
 
 module.exports = {installDeps};
