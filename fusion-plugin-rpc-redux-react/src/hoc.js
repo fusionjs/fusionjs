a3582d3d5efafac188bfddaf9b42822683736d20
Mickey Burks <mburks@uber.com>
2019-06-05 10:15:25 -0700

    Squashed commit of the following:
    commit 8095f492c9d8bda989ca91cae6f91f88070f488f
    Author: Mickey Burks <mburks@uber.com>
    Date:   Tue May 28 11:22:36 2019 -0700
    
        Add migration doc
    
    commit 1bac073ac5c1a581955a178cd564879cd553b5c0
    Author: Mickey Burks <mburks@uber.com>
    Date:   Wed May 22 14:27:10 2019 -0700
    
        Remove hook
    
    commit 92256e4039a9df462e63972cb06e9a4a94f9cf18
    Author: Mickey Burks <mburks@uber.com>
    Date:   Wed May 22 13:48:25 2019 -0700
    
        Fix lint
    
    commit e92879699eb2583f467304174e7a950c813b024a
    Author: Mickey Burks <mburks@uber.com>
    Date:   Wed May 22 13:17:38 2019 -0700
    
        Ignore .fusion
    
    commit 643d7cc46c691222e6193dcc24fd18fff6a70e74
    Author: Mickey Burks <mburks@uber.com>
    Date:   Wed May 22 12:44:47 2019 -0700
    
        Update tests to use jest, add fixture
    
    commit 5fd701263b1f0816d2ba40acaa4f3fd774a4a805
    Author: Mickey Burks <mburks@uber.com>
    Date:   Tue May 21 17:14:16 2019 -0700
    
        Get e2e tests to work
    
    commit c33d1f1d377396b886f0ff368b1c875267d5ba50
    Author: Mickey Burks <mburks@uber.com>
    Date:   Mon May 20 15:40:55 2019 -0700
    
        Fix files
    
    commit ef709be67b165c0083d6ea28263b275b34fb634a
    Author: Mickey Burks <mburks@uber.com>
    Date:   Mon May 20 15:31:59 2019 -0700
    
        Fix lint issues
    
    commit 2a697d4f59ff2e079cebde6dc981f4b495211eb0
    Author: Mickey Burks <mburks@uber.com>
    Date:   Fri May 17 15:28:55 2019 -0700
    
        Upgrade to cupv4
    
    commit b68af33ec594940ecadbc4f30fcb2b7303f7f265
    Author: Mickey Burks <mburks@uber.com>
    Date:   Mon May 20 14:03:26 2019 -0700
    
        Fix lint
    
    commit c3d6a8c3c442a6f243cf418f090b5265ee7a8824
    Author: Mickey Burks <mburks@uber.com>
    Date:   Fri May 17 17:37:48 2019 -0700
    
        Remove backup file
    
    commit 7baa35a5b0397e592ba4769496bcd1c858e6f2a4
    Author: Mickey Burks <mburks@uber.com>
    Date:   Fri May 17 17:34:30 2019 -0700
    
        Migrate tests from using legacy context
    
    commit 0e7adbed2c345b800bdee3e1e58e9b5f45c89169
    Author: Mickey Burks <mburks@uber.com>
    Date:   Fri May 17 14:00:45 2019 -0700
    
        Update README, add hook usage and example, remove docs regarding plugin registration
    
    commit cc513d5eb4025788e89c6ed0b3702b83015d173d
    Author: Mickey Burks <mburks@uber.com>
    Date:   Fri May 17 14:00:04 2019 -0700
    
        Fix peerDependencies
    
    commit f2652e3ad66740d805f450a25c41b53c465df1e9
    Author: Mickey Burks <mburks@uber.com>
    Date:   Fri May 17 13:59:50 2019 -0700
    
        Add useRPCHandler hook, refactor hoc to use hook
    
    commit f37fc7c7503d87cea0d40eee56832bf4ced4fb8e
    Author: Mickey Burks <mburks@uber.com>
    Date:   Fri May 17 13:58:47 2019 -0700
    
        Remove ProviderPlugin override
    
    commit d7c91ef3ea8993c56ea890af76e8fa99fadeb588
    Author: Mickey Burks <mburks@uber.com>
    Date:   Fri May 17 13:57:18 2019 -0700
    
        Remove logs
    
    commit 4e81906192efd887f664634de1efefbe3afed4fa
    Author: Mickey Burks <mburks@uber.com>
    Date:   Thu May 16 11:32:54 2019 -0700
    
        WIP

--- fusion-plugin-rpc-redux-react/src/hoc.js
+++ fusion-plugin-rpc-redux-react/src/hoc.js
@@ -6,16 +6,19 @@
  * @flow
  */
 
-import PropTypes from 'prop-types';
 import * as React from 'react';
 import type {Reducer} from 'redux';
-import {createRPCHandler, createRPCReactors} from 'fusion-rpc-redux';
+import {RPCToken} from 'fusion-plugin-rpc';
+import {ReduxToken} from 'fusion-plugin-react-redux';
+import {FusionContext, useService} from 'fusion-react';
+import {createRPCReactors, createRPCHandler} from 'fusion-rpc-redux';
 
 type RPCReducersType = {
   start?: Reducer<*, *>,
   success?: Reducer<*, *>,
   failure?: Reducer<*, *>,
 };
+
 export function withRPCReactor<Props: {}>(
   rpcId: string,
   reducers: RPCReducersType,
@@ -53,34 +56,25 @@ export function withRPCRedux<Props: {}>(
   } = {}
 ): (React.ComponentType<*>) => React.ComponentType<*> {
   return (Component: React.ComponentType<Props>) => {
-    class withRPCRedux extends React.Component<Props, *> {
-      render() {
-        const {rpc, store} = this.context;
-
-        const wrappedMapStateToParams =
-          mapStateToParams &&
-          ((state, args) => mapStateToParams(state, args, this.props));
-        const handler = createRPCHandler({
-          rpcId,
-          rpc,
-          store,
-          actions,
-          mapStateToParams: wrappedMapStateToParams,
-          transformParams,
-        });
-        const props = {
-          ...this.props,
-          [propName]: handler,
-        };
-        return React.createElement(Component, props);
-      }
+    function WithRPCRedux(props: Props) {
+      const ctx = React.useContext(FusionContext);
+      const {store} = useService(ReduxToken).from(ctx);
+      const rpc = useService(RPCToken).from(ctx);
+      const wrappedMapStateToParams =
+        mapStateToParams &&
+        ((state, args) => mapStateToParams(state, args, this.props));
+      const handler = createRPCHandler({
+        rpcId,
+        rpc,
+        store,
+        actions,
+        mapStateToParams: wrappedMapStateToParams,
+        transformParams,
+      });
+      return React.createElement(Component, {...props, [propName]: handler});
     }
     const displayName = Component.displayName || Component.name || 'Anonymous';
-    withRPCRedux.displayName = 'WithRPCRedux' + '(' + displayName + ')';
-    withRPCRedux.contextTypes = {
-      rpc: PropTypes.object.isRequired,
-      store: PropTypes.object.isRequired,
-    };
-    return withRPCRedux;
+    WithRPCRedux.displayName = 'WithRPCRedux' + '(' + displayName + ')';
+    return WithRPCRedux;
   };
 }
