66535adb3d2c50b58125d44d8f6988920b2256b1
Mickey Burks <mburks@uber.com>
2019-06-06 14:59:36 -0700

    Update fusion-cli tests

--- fusion-cli/test/e2e/split-translations/fixture/src/hookTranslation.js
+++ fusion-cli/test/e2e/split-translations/fixture/src/hookTranslation.js
@@ -4,15 +4,7 @@ import React from 'react';
 import {useTranslations} from 'fusion-plugin-i18n-react';
 
 export default () => {
-  const translate = useTranslations(['pattern.*', 'translations.hook']);
-  /*
-  useTranslations(['pattern.*', 'translations.hook']);
   const translate = useTranslations();
-  */
-  /*
-  useTranslations(['pattern.*', 'translations.hook']);
-  const {translate} = useService(I18nToken).from(React.useContext(FusionContext));
-  */
   return (
     <div id="hook-translation">
       <p>
