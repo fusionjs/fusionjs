7f8b0e18ec6fbb7d7c48b0ee1ea88fb60c6425cf
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-06-26 16:34:28 -0600

    Fix spurious client-side build warning
    https://github.com/fusionjs/fusionjs/pull/482
    
    Co-authored-by: Ryan Tsao <ryan.j.tsao@gmail.com>

--- fusion-react/src/async/prepare.js
+++ fusion-react/src/async/prepare.js
@@ -7,7 +7,7 @@
  */
 
 import * as React from 'react';
-import ssrPrepass from 'react-ssr-prepass';
+import ssrPrepass from '@rtsao/react-ssr-prepass';
 
 class PrepareState {
   seen: Map<any, Set<string>>;
