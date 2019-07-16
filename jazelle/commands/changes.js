af5aa27694e04c8cb2acfa8be8bfa2f909047fed
Joseph Agnelli <jagnelli@uber.com>
2019-07-16 16:21:19 +0000

    Merge branch 'master' into uber/fusionjs/295
    meta:sha:3e6a78251e72d60fba1e29872900310edc70abdf

--- jazelle/commands/changes.js
+++ jazelle/commands/changes.js
@@ -4,11 +4,12 @@ const {findChangedTargets} = require('../utils/find-changed-targets.js');
 /*::
 type ChangesArgs = {
   root: string,
+  type: string,
 };
 type Changes = (ChangesArgs) => Promise<void>;
 */
-const changes /*: Changes */ = async ({root}) => {
-  const targets = await findChangedTargets({root});
+const changes /*: Changes */ = async ({root, type}) => {
+  const targets = await findChangedTargets({root, type});
   console.log(targets.join('\n'));
 };
 
--- jazelle/utils/find-changed-targets.js
+++ jazelle/utils/find-changed-targets.js
@@ -1,35 +1,78 @@
 // @flow
-const {exec} = require('../utils/node-helpers.js');
-const {bazel} = require('../utils/binary-paths.js');
+const {getManifest} = require('./get-manifest.js');
+const {exec} = require('./node-helpers.js');
+const {bazel} = require('./binary-paths.js');
 
 /*::
 export type FindChangedTargetsArgs = {
   root: string,
+  type: string,
 };
 export type FindChangedTargets = (FindChangedTargetsArgs) => Promise<Array<string>>;
 */
-const findChangedTargets /*: FindChangedTargets */ = async ({root}) => {
+const findChangedTargets /*: FindChangedTargets */ = async ({root, type}) => {
+  const targets = await findChangedBazelTargets({root});
+  switch (type) {
+    case 'bazel':
+      return targets;
+    case 'dirs':
+    default: {
+      const dirs = new Set();
+      for (const target of targets) {
+        dirs.add(target.slice(2, target.indexOf(':')));
+      }
+      return [...dirs];
+    }
+  }
+};
+
+const findChangedBazelTargets = async ({root}) => {
   const diff = await exec(`git diff-tree --no-commit-id --name-only -r HEAD`, {
     cwd: root,
   }).catch(() => null);
-  if (diff !== null) {
-    const files = diff.split('\n').filter(Boolean);
-    const projects = await batch(files, file => {
-      return `FILE=$(bazel query "${file}") && bazel query "attr('srcs', '$FILE', '\${FILE//:*/}:*')"`;
-    });
-    return batch(projects, project => {
-      return `${bazel} query 'let graph = kind(".*_test rule", rdeps("...", "${project}")) in $graph except filter("node_modules", $graph)'`;
-    });
+  const {projects, workspace} = await getManifest({root});
+  if (workspace === 'sandbox') {
+    if (diff !== null) {
+      const files = diff.split('\n').filter(Boolean);
+      const projects = await batch(files, file => {
+        return `FILE=$(${bazel} query "${file}") && bazel query "attr('srcs', '$FILE', '\${FILE//:*/}:*')"`;
+      });
+      return batch(projects, project => {
+        return `${bazel} query 'let graph = kind(".*_test rule", rdeps("...", "${project}")) in $graph except filter("node_modules", $graph)'`;
+      });
+    } else {
+      const queried = await exec(
+        `${bazel} query 'let graph = kind(".*_test rule", "...") in $graph except filter("node_modules", $graph)'`
+      );
+      return queried.trim().split('\n');
+    }
   } else {
-    const queried = await exec(
-      `${bazel} query 'let graph = kind(".*_test rule", "...") in $graph except filter("node_modules", $graph)'`
-    );
-    return queried.trim().split('\n');
+    const set = new Set();
+    if (diff !== null) {
+      for (const project of projects) {
+        for (const line of diff.split('\n').filter(Boolean)) {
+          if (line.startsWith(project)) set.add(project);
+        }
+      }
+    } else {
+      for (const project of projects) set.add(project);
+    }
+    const targets = [];
+    for (const project of set) {
+      targets.push(
+        `//${project}:test`,
+        `//${project}:lint`,
+        `//${project}:flow`
+      );
+    }
+    return targets;
   }
 };
 
 async function batch(items, fn) {
-  const stdouts = await Promise.all(items.map(item => exec(fn(item))));
+  const stdouts = await Promise.all(
+    items.map(item => exec(fn(item)).catch(() => ''))
+  );
   return [
     ...new Set(
       stdouts
