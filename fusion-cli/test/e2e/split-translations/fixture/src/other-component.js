cc0b453c41d892d43593fce94a4652fe648a9e47
Mickey Burks <mburks@uber.com>
2019-07-24 05:11:42 +0000

    Copy over work from previous PR
    meta:sha:9e175143df64813da65a5a0ce48c3f44e03f898b

---
+++ fusion-cli/test/e2e/split-translations/fixture/src/other-component.js
@@ -0,0 +1,10 @@
+// @noflow
+
+import React from 'react';
+import {Translate} from 'fusion-plugin-i18n-react';
+
+export default () => (
+  <div id="other-component">
+    <Translate id="other-component" />
+  </div>
+);
