4922f4c1cfd5049cb186dd4705a4245020ec287e
Alexander Smith <amsmith@uber.com>
2019-07-24 14:45:42 -0700

    Fix promise instrumentation (re-do) (#581)
    * Copy over work from previous PR
    
    meta:sha:9e175143df64813da65a5a0ce48c3f44e03f898b
    
    * Remove spaces
    
    meta:sha:471a10b52d7bd29c579402bed8e1ce0414e96870

--- fusion-cli/test/e2e/split/fixture/src/main.js
+++ fusion-cli/test/e2e/split/fixture/src/main.js
@@ -2,18 +2,33 @@
 import App from 'fusion-core';
 
 export default (async function() {
-  const app = new App('element', el => el);
   const aPromise = import('./test-a.js');
   const bPromise = import('./test-b.js');
-  const cPromise = import('./test.js');
+  const cPromise = import('./test-combined.js');
+  const dPromise = import('./test-transitive.js');
+
+  const app = new App(
+    'element',
+    el => {
+      if (__NODE__) {
+        return `<div id="ssr">${dPromise.__CHUNK_IDS}</div>`;
+      } else {
+        document.body.innerHTML = `<div id="csr">${dPromise.__CHUNK_IDS}</div>`;
+      }
+    }
+  );
+
   app.middleware((ctx, next) => {
     if (ctx.path === '/test-a') {
       ctx.body = aPromise.__CHUNK_IDS;
     } else if (ctx.path === '/test-b') {
       ctx.body = bPromise.__CHUNK_IDS;
-    } else if (ctx.path === '/test') {
+    } else if (ctx.path === '/test-combined') {
       ctx.body = cPromise.__CHUNK_IDS;
+    } else if (ctx.path === '/test-transitive') {
+      ctx.body = dPromise.__CHUNK_IDS;
     }
+    return next();
   });
   return app;
 });
