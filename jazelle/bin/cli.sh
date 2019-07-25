4000734704890d172813214107ca5be288716643
Leo Horie <lhorie@uber.com>
2019-07-25 21:15:37 +0000

    don't error if package.json doesn't exist
    meta:sha:7ee6da6f13633b485e043d3a72e24af04ac43fdf

--- jazelle/bin/cli.sh
+++ jazelle/bin/cli.sh
@@ -8,7 +8,7 @@ fi
 
 # find project root
 findroot() {
-  if [ -f "WORKSPACE" ] && grep -v "\"name\": \"jazelle\"" "$PWD/package.json"
+  if [ -f "WORKSPACE" ] && grep -s -v "\"name\": \"jazelle\"" "$PWD/package.json"
   then
     echo "$PWD/"
   elif [ "$PWD" = "/" ]
