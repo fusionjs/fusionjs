23e8c1aee4b8fde5efd2fa774738c8ea110582d4
Joseph Agnelli <jagnelli@uber.com>
2019-08-02 23:37:22 +0000

    Check for the existence of the correct file in test
    meta:sha:114a2e7718e6e63cf8ceebac8cf257d37a4ba759

--- fusion-cli/test/e2e/brotli-zopfli/test.js
+++ fusion-cli/test/e2e/brotli-zopfli/test.js
@@ -3,12 +3,12 @@
 
 const path = require('path');
 const fs = require('fs');
-const {promisify} = require('util');
+const { promisify } = require('util');
 const t = require('assert');
 
 const exists = promisify(fs.exists);
 
-const {cmd} = require('../utils.js');
+const { cmd } = require('../utils.js');
 
 const dir = path.resolve(__dirname, './fixture');
 const dir2 = path.resolve(__dirname, './fixture2');
@@ -18,7 +18,7 @@ test('respects .fusionrc for zopfli and brotli', async () => {
 
   const clientMainPathGZ = path.resolve(
     dir,
-    `.fusion/dist/production/client/client-main-c9d5e15139c37dc8d00d.js.gz`
+    `.fusion/dist/production/client/client-main-1a6976e349e3456d7e1e.js.gz`
   );
 
   t.ok(
@@ -28,7 +28,7 @@ test('respects .fusionrc for zopfli and brotli', async () => {
 
   const clientMainPathBR = path.resolve(
     dir,
-    `.fusion/dist/production/client/client-main-c9d5e15139c37dc8d00d.js.br`
+    `.fusion/dist/production/client/client-main-1a6976e349e3456d7e1e.js.br`
   );
 
   t.ok(
@@ -40,14 +40,14 @@ test('respects .fusionrc for zopfli and brotli', async () => {
 
   const clientMainPathGZ2 = path.resolve(
     dir2,
-    `.fusion/dist/production/client/client-main-69fef501aaf7ef73ab62.js.gz`
+    `.fusion/dist/production/client/client-main-1a6976e349e3456d7e1e.js.gz`
   );
 
   t.ok(await exists(clientMainPathGZ2), "Zopfli Wasn't Made, but should have");
 
   const clientMainPathBR2 = path.resolve(
     dir2,
-    `.fusion/dist/production/client/client-main-69fef501aaf7ef73ab62.js.br`
+    `.fusion/dist/production/client/client-main-1a6976e349e3456d7e1e.js.br`
   );
 
   t.ok(await exists(clientMainPathBR2), "Brotli Wasn't Made, but should have");
