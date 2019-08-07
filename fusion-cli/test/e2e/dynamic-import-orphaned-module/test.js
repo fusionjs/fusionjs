1c8082e83653d7eff253624ac21692ee320e6260
Mickey Burks <brks.mck@gmail.com>
2019-08-07 23:55:20 +0000

    Add test for chunkGroup check
    meta:sha:e78a1381d3acb394a61511ab89ad8b3f830731bc

---
+++ fusion-cli/test/e2e/dynamic-import-orphaned-module/test.js
@@ -0,0 +1,27 @@
+// @flow
+/* eslint-env node */
+
+const t = require('assert');
+const fs = require('fs');
+const path = require('path');
+const {promisify} = require('util');
+const {cmd} = require('../utils.js');
+
+const readdir = promisify(fs.readdir);
+const dir = path.resolve(__dirname, './fixture');
+
+test('`fusion build` with dynamic imports', async () => {
+  await cmd(`build --dir=${dir}`);
+
+  // Build completes
+  t.ok(true);
+
+  const bundles = await readdir(
+    path.resolve(dir, '.fusion/dist/development/client')
+  );
+
+  const asyncBundles = bundles.filter(f => !/(vendor|runtime|main)/.test(f));
+
+  // No async bundles are created
+  t.equal(asyncBundles.length, 0);
+}, 100000);
