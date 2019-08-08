d34d0bd784ecea6298a7a4cd969a9a694a4596c9
Giancarlo Anemone <ganemone@uber.com>
2019-08-08 22:31:05 +0000

    Copy commit from parent repo
    meta:sha:186c9f26d32706cb6805c4397d9aa659f8b5f9e0

---
+++ fusion-cli/test/e2e/websocket-errors/fixture/src/main.js
@@ -0,0 +1,15 @@
+// @noflow
+import App, {HttpServerToken, createPlugin} from 'fusion-core';
+
+export default async function() {
+  const app = new App('test', el => el);
+  if (__NODE__) {
+    app.middleware((ctx, next) => {
+      if (ctx.url === '/health') {
+        ctx.body = 'OK';
+      }
+      return next();
+    });
+  }
+  return app;
+}
