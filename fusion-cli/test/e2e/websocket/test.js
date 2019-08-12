b54feac65f548c581ac1474a45de25484a6475fe
UberOpenSourceBot <33560951+UberOpenSourceBot@users.noreply.github.com>
2019-08-12 12:27:33 -0700

    Fix bug with handling web socket request failures in dev (#627)
    https://github.com/fusionjs/fusionjs/pull/627

---
+++ fusion-cli/test/e2e/websocket/test.js
@@ -0,0 +1,50 @@
+// @flow
+/* eslint-env node */
+
+const path = require('path');
+const WebSocket = require('ws');
+
+const {dev, cmd, start} = require('../utils.js');
+
+const dir = path.resolve(__dirname, './fixture');
+
+jest.setTimeout(200000);
+
+test('`fusion dev` works with websockets', async () => {
+  const {proc, port} = await dev(`--dir=${dir}`);
+  await new Promise(resolve => {
+    const client = new WebSocket(`ws://localhost:${port}/`);
+    client.on('open', () => {
+      client.send('hello');
+    });
+    client.on('error', e => {
+      throw e;
+    });
+    client.on('message', message => {
+      expect(message).toEqual('hello');
+      client.close();
+      proc.kill();
+      resolve();
+    });
+  });
+});
+
+test('`fusion build/start` works with websockets', async () => {
+  await cmd(`build --dir=${dir}`);
+  const {proc, port} = await start(`--dir=${dir}`);
+  await new Promise(resolve => {
+    const client = new WebSocket(`ws://localhost:${port}/`);
+    client.on('open', () => {
+      client.send('hello');
+    });
+    client.on('error', e => {
+      throw e;
+    });
+    client.on('message', message => {
+      expect(message).toEqual('hello');
+      client.close();
+      proc.kill();
+      resolve();
+    });
+  });
+});
