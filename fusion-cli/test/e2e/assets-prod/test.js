7f8a9d9d2361b21582f711c9beab93e77cea4987
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-08-16 17:29:29 +0000

    Update test descriptions
    meta:sha:98e86abe1d901694d9c8122d7bf4b6a95078384d

--- fusion-cli/test/e2e/assets-prod/test.js
+++ fusion-cli/test/e2e/assets-prod/test.js
@@ -15,7 +15,7 @@ const {cmd, start} = require('../utils.js');
 
 const dir = path.resolve(__dirname, './fixture');
 
-test('`fusion build` with assets', async () => {
+test('source map static assets are not served in production', async () => {
   const env = {...process.env, NODE_ENV: 'production'};
   await cmd(`build --dir=${dir} --production`, {env});
   const jsPath = '/_static/client-main-2a2dcf4bf566f64f25b0.js';
@@ -27,13 +27,14 @@ test('`fusion build` with assets', async () => {
       resolveWithFullResponse: true,
       simple: false,
     });
-    t.equal(asset.statusCode, 200);
+    t.equal(asset.statusCode, 200, "Request for JS bundle yields OK response");
 
     const assetMap = await request(`http://localhost:${port}${mapPath}`, {
       resolveWithFullResponse: true,
       simple: false,
     });
-    t.equal(assetMap.statusCode, 404);
+    t.equal(assetMap.statusCode, 404, "Request for associated source map 404s");
+    t.equal(assetMap.body, "Not Found");
 
     proc.kill();
   } catch (e) {
