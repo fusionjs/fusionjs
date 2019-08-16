4942046f2c9719502836be592a3d5514067ad947
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-08-16 17:35:27 +0000

    Don't hardcode asset paths
    meta:sha:a19c7c38d25ea5519a322e468d925664122a369d

--- fusion-cli/test/e2e/assets-prod/test.js
+++ fusion-cli/test/e2e/assets-prod/test.js
@@ -18,23 +18,40 @@ const dir = path.resolve(__dirname, './fixture');
 test('source map static assets are not served in production', async () => {
   const env = {...process.env, NODE_ENV: 'production'};
   await cmd(`build --dir=${dir} --production`, {env});
-  const jsPath = '/_static/client-main-2a2dcf4bf566f64f25b0.js';
-  const mapPath = `${jsPath}.map`;
+
   try {
     const {proc, port} = await start(`--dir=${dir}`, {env});
 
-    const asset = await request(`http://localhost:${port}${jsPath}`, {
-      resolveWithFullResponse: true,
-      simple: false,
-    });
-    t.equal(asset.statusCode, 200, "Request for JS bundle yields OK response");
+    const bundles = fs
+      .readdirSync(path.join(dir, '.fusion/dist/production/client'))
+      .filter(file => path.extname(file) === '.js');
+    t.ok(bundles.length > 0);
 
-    const assetMap = await request(`http://localhost:${port}${mapPath}`, {
-      resolveWithFullResponse: true,
-      simple: false,
-    });
-    t.equal(assetMap.statusCode, 404, "Request for associated source map 404s");
-    t.equal(assetMap.body, "Not Found");
+    for (const bundle of bundles) {
+      const jsPath = `/_static/${bundle}`;
+      const mapPath = `${jsPath}.map`;
+
+      const asset = await request(`http://localhost:${port}${jsPath}`, {
+        resolveWithFullResponse: true,
+        simple: false,
+      });
+      t.equal(
+        asset.statusCode,
+        200,
+        'Request for JS bundle yields OK response'
+      );
+
+      const assetMap = await request(`http://localhost:${port}${mapPath}`, {
+        resolveWithFullResponse: true,
+        simple: false,
+      });
+      t.equal(
+        assetMap.statusCode,
+        404,
+        'Request for associated source map 404s'
+      );
+      t.equal(assetMap.body, 'Not Found');
+    }
 
     proc.kill();
   } catch (e) {
