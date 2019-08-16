f62544f4f1424c15cda3c7acb02441bbdbbdaf80
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-08-16 21:42:51 +0000

    Update test
    meta:sha:4f6fd63f255d97db664f8b1ae49ac90ec5263ec0

--- fusion-cli/test/e2e/assets-prod/fixture/src/main.js
+++ fusion-cli/test/e2e/assets-prod/fixture/src/main.js
@@ -1,7 +1,16 @@
 // @noflow
 import App, {assetUrl} from 'fusion-core';
 
+const url = assetUrl('./custom.js.map');
+
 export default (async function() {
   const app = new App('element', el => el);
+  app.middleware((ctx, next) => {
+    if (ctx.url === '/asset-url') {
+      ctx.body = url;
+    }
+    return next();
+  });
+
   return app;
 });
