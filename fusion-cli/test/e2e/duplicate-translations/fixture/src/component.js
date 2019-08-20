519bcc22a2bcf89ce6c2edc10a9e71de8ed05ce8
Mickey Burks <mburks@uber.com>
2019-08-20 01:56:10 +0000

    Deduplicate translation keys in promise instrumentation
    meta:sha:612356b5ca0a8a2d4c520dfbb8fed762b94225f3

---
+++ fusion-cli/test/e2e/duplicate-translations/fixture/src/component.js
@@ -0,0 +1,12 @@
+// @noflow
+
+import React from 'react';
+import {Translate} from 'fusion-plugin-i18n-react';
+
+export default () => (
+  <div>
+    <Translate id="main" />
+    <Translate id="main" />
+    <Translate id="main" />
+  </div>
+);
