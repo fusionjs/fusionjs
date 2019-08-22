68b5d2701718bad137c9904e633acddc6d6c4671
UberOpenSourceBot <33560951+UberOpenSourceBot@users.noreply.github.com>
2019-08-22 12:45:48 -0700

    Replace local libdefs with official flow-typed defs (#651)
    https://github.com/fusionjs/fusionjs/pull/651
    
    meta:sha:f15b8512e912383ac7408985239e21df4dba6be2
    
    Co-authored-by: Mickey Burks <mburks@uber.com>

--- flow-typed/npm/just-safe-get_v1.3.x.js
+++ flow-typed/npm/just-safe-get_v1.3.x.js
@@ -1,4 +1,5 @@
-// @flow
+// flow-typed signature: 8a3ff877cc20f65ecbd3170dc67175bb
+// flow-typed version: c6154227d1/just-safe-get_v1.3.x/flow_>=v0.98.x <=v0.103.x
 
 declare module 'just-safe-get' {
   declare export default function get(
