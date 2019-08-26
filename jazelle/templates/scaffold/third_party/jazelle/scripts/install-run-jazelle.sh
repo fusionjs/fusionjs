33f1481619b634bac5cb9df329cc403b4587ec97
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-08-26 17:46:16 +0000

    Merge branch 'master' into uber/fusionjs/421
    meta:sha:e00b2d391cdf5bd479878552cda53edc959b112f

--- jazelle/templates/scaffold/third_party/jazelle/scripts/install-run-jazelle.sh
+++ jazelle/templates/scaffold/third_party/jazelle/scripts/install-run-jazelle.sh
@@ -18,7 +18,6 @@ ROOT=$(findroot)
 VERSION=$(grep -E -o "jazelle-(.+).tgz" "$ROOT/WORKSPACE")
 VERSION=${VERSION:8:${#VERSION}-12}
 BIN=$ROOT/third_party/jazelle/temp
-mkdir -p $ROOT/third_party/jazelle/temp
 TARBALL=$BIN/jazelle.tgz
 JAZELLE=$BIN/package/bin/cli.sh
 if [ ! -f $JAZELLE ]
