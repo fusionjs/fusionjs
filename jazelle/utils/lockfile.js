44a0d425bc874c250b2229b465d5c36a1628901b
UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
2019-09-09 21:26:36 +0000

    Improve lockfile change detection and error message
    Improves logic and error messages around lockfile validation
    
    Co-authored-by: Chris Deacy <deacy@uber.com>
    Co-authored-by: Victor Powell <vp@uber.com>

--- jazelle/utils/lockfile.js
+++ jazelle/utils/lockfile.js
@@ -435,14 +435,23 @@ const update /*: Update */ = async ({
       const oldKeys = Object.keys(item.lockfile);
       const newKeys = Object.keys(lockfile);
       if (oldKeys.sort().join() !== newKeys.sort().join()) {
-        console.error(
-          `Updating lockfile is not allowed with frozenLockfile. ` +
-            `This error is most likely happening if you have committed ` +
-            `out-of-date lockfiles and tried to install deps in CI. ` +
-            `Install your deps again locally.\n\n` +
-            `OLD: ${oldKeys.sort().join()}\n\n` +
-            `NEW: ${newKeys.sort().join()}`
-        );
+        // if newKeys is a subset of oldKeys, we're pruning
+        // dependencies which is OK.
+        const missingKeys = [];
+        for (let key of newKeys) {
+          if (!item.lockfile[key]) {
+            missingKeys.push(key);
+          }
+        }
+        if (missingKeys.length) {
+          console.error(
+            `Updating lockfile is not allowed with frozenLockfile. ` +
+              `This error is most likely happening if you have committed ` +
+              `out-of-date lockfiles and tried to install deps in CI. ` +
+              `Install your deps again locally.\n\n` +
+              `missing keys: ${missingKeys.slice(0, 10).join()}`
+          );
+        }
       }
     } else {
       item.lockfile = lockfile;
