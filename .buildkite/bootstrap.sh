059d1dd1ea7b3921f819bd4a703991fc7aca8b0e
Chris Deacy <deacy@uber.com>
2019-09-17 13:17:57 -0500

    dump wip

--- .buildkite/bootstrap.sh
+++ .buildkite/bootstrap.sh
@@ -7,6 +7,10 @@ SHA2="$(git rev-parse origin/master)"
 git diff-tree --no-commit-id --name-only -r $SHA1 $SHA2 > changes.txt
 CHANGES=$(jazelle changes ./changes.txt)
 
+has_cover_script() {
+  #
+}
+
 for DIR in $CHANGES ; do (
   PROJECT=$(basename "$DIR");
   if [ -d "$DIR" ] && [ $PROJECT != "common" ] && [ $PROJECT != "scripts" ] && [ $PROJECT != "flow-typed" ] && [ $PROJECT != "rfcs" ] && [ $PROJECT != "docs" ]; then
@@ -16,6 +20,9 @@ for DIR in $CHANGES ; do (
       echo "    - 'cd fusion-cli'";
       echo "    - 'jazelle ci'";
       echo "    - 'jazelle build'";
+      # TODO: figure out how to get codecov working for fusion-cli. currently
+      # runs as multiple different jobs, so don't have one place to check for
+      # coverage report
       echo "    - '.buildkite/nodeTests'";
       echo "    parallelism: 10";
       echo "    timeout_in_minutes: 10";
@@ -29,7 +36,11 @@ for DIR in $CHANGES ; do (
       echo "    commands:";
       echo "    - 'jazelle ci --cwd=$DIR'";
       echo "    - 'jazelle build --cwd=$DIR'";
-      echo "    - 'jazelle test --cwd=$DIR'";
+      if []; then
+        echo "    - 'jazelle cover --cwd=$DIR'";
+      else
+        echo "    - 'jazelle test --cwd=$DIR'";
+      fi;
       echo "    - 'jazelle lint --cwd=$DIR'";
       echo "    - 'jazelle flow --cwd=$DIR'";
       echo "    timeout_in_minutes: 10";
