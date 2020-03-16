9104771cffe1c7c3b658a4d73c4db880e1eac48d
Kevin Grandon <kevingrandon@yahoo.com>
2020-03-16 20:37:40 +0000

    Revert "Ensure that flow output contains colors (#919)"

--- jazelle/rules/flow-check.js
+++ jazelle/rules/flow-check.js
@@ -15,7 +15,7 @@ if (exists(`${main}/node_modules/.bin/flow`)) {
     exec(`tar xzf "${f}" -C "${target}"`, {cwd: bin});
   });
 
-  const params = ['--color=always', ...args.map(arg => `'${arg}'`)].join(' ');
+  const params = args.map(arg => `'${arg}'`).join(' ');
   const cmd = `${node} ${main}/node_modules/.bin/flow ${params}`;
   const dir = dirname(realpath(`${main}/package.json`));
   exec(cmd, {cwd: dir, env: process.env, stdio: 'inherit'});
