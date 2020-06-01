d47867faeb3bb8b03f9c8df534217daffe99ff8d
Mickey Burks <brks.mck@gmail.com>
2020-06-01 22:00:58 +0000

    Add line position to babel-plugin-i18n errors
    Add line position to `babel-plugin-i18n` static analysis errors
    
    Mentioned in https://github.com/fusionjs/fusionjs/issues/737
    
    Before:
    ```
    Module build failed (from ./node_modules/fusion-cli/build/loaders/babel-loader.js):
    Error: ---/src/components/app.js: useTranslations result function must be passed string literal or hinted template literal
        at ---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-i18n/index.js:81:23
        at Array.forEach (<anonymous>)
        at ---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-i18n/index.js:61:28
        at Array.forEach (<anonymous>)
        at refsHandler (---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-i18n/index.js:36:10)
        at ---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-utils/visit-named-module.js:57:13
        at Array.forEach (<anonymous>)
        at PluginPass.ImportDeclaration (---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-utils/visit-named-module.js:45:30)
        at newFn (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/visitors.js:179:21)
        at NodePath._call (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/path/context.js:55:20)
        at NodePath.call (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/path/context.js:42:17)
        at NodePath.visit (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/path/context.js:90:31)
        at TraversalContext.visitQueue (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/context.js:112:16)
        at TraversalContext.visitMultiple (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/context.js:79:17)
        at TraversalContext.visit (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/context.js:138:19)
        at Function.traverse.node (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/index.js:84:17)
    ```
    
    After:
    ```
    Module build failed (from ./node_modules/fusion-cli/build/loaders/babel-loader.js):
    SyntaxError: ---/src/components/app.js: useTranslations result function must be passed string literal or hinted template literal
      16 | export default function App() {
      17 |   const t = useTranslations();
    > 18 |   t();
         |   ^^^
      19 |   return (
      20 |     <Switch>
        at File.buildCodeFrameError (---/node_modules/@babel/core/lib/transformation/file/file.js:267:12)
        at NodePath.buildCodeFrameError (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/path/index.js:144:21)
        at ---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-i18n/index.js:92:34
        at Array.forEach (<anonymous>)
        at ---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-i18n/index.js:70:28
        at Array.forEach (<anonymous>)
        at refsHandler (---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-i18n/index.js:36:10)
        at ---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-utils/visit-named-module.js:57:13
        at Array.forEach (<anonymous>)
        at PluginPass.ImportDeclaration (---/node_modules/fusion-cli/build/babel-plugins/babel-plugin-utils/visit-named-module.js:45:30)
        at newFn (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/visitors.js:179:21)
        at NodePath._call (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/path/context.js:55:20)
        at NodePath.call (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/path/context.js:42:17)
        at NodePath.visit (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/path/context.js:90:31)
        at TraversalContext.visitQueue (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/context.js:112:16)
        at TraversalContext.visitMultiple (---/node_modules/@babel/core/node_modules/@babel/traverse/lib/context.js:79:17)
    ```

--- fusion-cli/build/babel-plugins/babel-plugin-i18n/index.js
+++ fusion-cli/build/babel-plugins/babel-plugin-i18n/index.js
@@ -34,24 +34,25 @@ function i18nPlugin(babel /*: Object */, {translationIds} /*: PluginOpts */) {
 
   function refsHandler(t, context, refs = [], specifierName) {
     refs.forEach(refPath => {
+      const parentPath = refPath.parentPath;
       if (t.isCallExpression(refPath.parent)) {
         const firstArg = refPath.parent.arguments[0];
         if (specifierName === 'withTranslations') {
           const errorMessage =
             'The withTranslations hoc must be called with an array of string literal translation keys';
           if (!t.isArrayExpression(firstArg)) {
-            throw new Error(errorMessage);
+            throw parentPath.buildCodeFrameError(errorMessage);
           }
           const elements = firstArg.elements;
           elements.forEach(element => {
             if (!t.isStringLiteral(element)) {
-              throw new Error(errorMessage);
+              throw parentPath.buildCodeFrameError(errorMessage);
             }
             translationIds.add(element.value);
           });
         } else if (specifierName === 'useTranslations') {
           if (!t.isVariableDeclarator(refPath.parentPath.parent)) {
-            throw new Error(
+            throw parentPath.buildCodeFrameError(
               'Unexpected assignment of useTranslations return function'
             );
           }
@@ -73,12 +74,16 @@ function i18nPlugin(babel /*: Object */, {translationIds} /*: PluginOpts */) {
                 const literalSections = arg.quasis.map(q => q.value.cooked);
                 if (literalSections.join('') === '') {
                   // template literal not hinted, i.e. translate(`${foo}`)
-                  throw new Error(errorMessage);
+                  throw translationPath.parentPath.buildCodeFrameError(
+                    errorMessage
+                  );
                 } else {
                   translationIds.add(literalSections);
                 }
               } else {
-                throw new Error(errorMessage);
+                throw translationPath.parentPath.buildCodeFrameError(
+                  errorMessage
+                );
               }
             } else if (
               // React.useEffect(() => {}, [translate])
@@ -105,12 +110,12 @@ function i18nPlugin(babel /*: Object */, {translationIds} /*: PluginOpts */) {
               if (
                 !(isSecondArg && (isValidIdentifierCall || isValidMemberCall))
               ) {
-                throw new Error(
+                throw translationPath.parentPath.buildCodeFrameError(
                   'Unexpected usage of useTranslations return function'
                 );
               }
             } else {
-              throw new Error(
+              throw translationPath.parentPath.buildCodeFrameError(
                 'Unexpected usage of useTranslations return function'
               );
             }
@@ -132,7 +137,7 @@ function i18nPlugin(babel /*: Object */, {translationIds} /*: PluginOpts */) {
           return;
         }
         if (!t.isStringLiteral(attr.value)) {
-          throw new Error(
+          throw parentPath.buildCodeFrameError(
             'The translate component must have props.id be a string literal.'
           );
         }
