7b384e50cfe006e8d74c4eb2b6654edb5425ffa6
Leo Horie <lhorie@uber.com>
2020-07-06 23:50:22 +0000

    Copy dist files back to source folder only if they haven't changed
    Rather than always overwriting `dist`, only do so if the contents haven't changed. This is required, for example, in cases like if `dist` is `.flowconfig`, since touching this file causes flow server to restart.

--- jazelle/rules/untar.js
+++ jazelle/rules/untar.js
@@ -1,5 +1,5 @@
 // @flow
-const {realpathSync: realpath} = require('fs');
+const {realpathSync: realpath, statSync: stat, readFileSync} = require('fs');
 const {execSync: exec} = require('child_process');
 const {dirname, resolve} = require('path');
 
@@ -14,7 +14,7 @@ const files = exec(`find . -name __jazelle__*.tgz`, options)
 files.map(file => {
   untarIntoSandbox(file);
   if (runtime) {
-    untarToSourceFolder(file);
+    copyToSourceFolder(file);
   }
 });
 
@@ -24,9 +24,31 @@ function untarIntoSandbox(file) {
   exec(untar, {cwd: root});
 }
 
-function untarToSourceFolder(file) {
+function copyToSourceFolder(file) {
   const target = resolve(root, dirname(file));
   const real = dirname(realpath(`${target}/package.json`));
-  const untar = `tar xzf "${file}" -C "${real}"`;
-  exec(untar, {cwd: root});
+  const files = exec(`tar ztf ${file}`, {encoding: 'utf8'})
+    .trim()
+    .split('\n')
+    .map(line => line.replace(/\/$/, ''));
+  for (const file of files) {
+    if (stat(`${target}/${file}`).isDirectory()) {
+      exec(
+        `mkdir -p ${real}/${file} && cp -rf ${target}/${file}/ ${real}/${file}`
+      );
+    } else {
+      // only overwrite file if it's not identical
+      if (read(`${target}/${file}`) !== read(`${real}/${file}`)) {
+        exec(`cp -rf ${target}/${file} ${real}/${file}`);
+      }
+    }
+  }
+}
+
+function read(file) {
+  try {
+    return readFileSync(file, 'utf8');
+  } catch (e) {
+    return Symbol('not found'); // must return something that does not equal itself
+  }
 }
