312e2c0c168987e1cec1bd7ac1138b0d4ffd8346
Ryan Tsao <rtsao@uber.com>
2022-03-11 17:01:51 +0000

    Make i18n plugin tests more robust
    Use `beforeEach`/`afterEach` to ensure test fixtures aren't left in a modified state if tests fail

--- fusion-plugin-i18n/__tests__/loader.node.js
+++ fusion-plugin-i18n/__tests__/loader.node.js
@@ -18,56 +18,70 @@ import I18n from '../src/node';
 import createLoader from '../src/loader';
 import {createI18nLoader, I18nLoaderToken, I18nToken} from '../src/index.js';
 
-test('loader', async () => {
-  fs.mkdirSync('translations');
-  fs.writeFileSync('translations/en_US.json', '{"test": "hi ${value}"}');
+describe('loader', () => {
+  beforeEach(() => {
+    fs.mkdirSync('translations');
+    fs.writeFileSync('translations/en_US.json', '{"test": "hi ${value}"}');
+  });
 
-  const app = new App('el', (el) => el);
-  app.register(I18nToken, I18n);
-  // $FlowFixMe
-  app.register(UniversalEventsToken, {
-    from: () => ({emit: () => {}}),
+  test('loader', async () => {
+    const app = new App('el', (el) => el);
+    app.register(I18nToken, I18n);
+    // $FlowFixMe
+    app.register(UniversalEventsToken, {
+      from: () => ({emit: () => {}}),
+    });
+    app.middleware({i18n: I18nToken}, ({i18n}) => {
+      return (ctx, next) => {
+        const translator = i18n.from(ctx);
+        expect(translator.translate('test', {value: 'world'})).toBe('hi world');
+        return next();
+      };
+    });
+    const simulator = getSimulator(app);
+    await simulator.render('/');
   });
-  app.middleware({i18n: I18nToken}, ({i18n}) => {
-    return (ctx, next) => {
-      const translator = i18n.from(ctx);
-      expect(translator.translate('test', {value: 'world'})).toBe('hi world');
-      return next();
-    };
+
+  afterEach(() => {
+    fs.unlinkSync('translations/en_US.json');
+    fs.rmdirSync('translations');
   });
-  const simulator = getSimulator(app);
-  await simulator.render('/');
-  fs.unlinkSync('translations/en_US.json');
-  fs.rmdirSync('translations');
 });
 
-test('custom locale resolver', async () => {
-  fs.mkdirSync('translations');
-  fs.writeFileSync('translations/en_US.json', '{"foo": "bar"}');
-  fs.writeFileSync('translations/custom_US.json', '{"foo": "qux"}');
+describe('resolver', () => {
+  beforeEach(() => {
+    fs.mkdirSync('translations');
+    fs.writeFileSync('translations/en_US.json', '{"foo": "bar"}');
+    fs.writeFileSync('translations/custom_US.json', '{"foo": "qux"}');
+  });
 
-  const app = new App('el', (el) => el);
-  app.register(
-    I18nLoaderToken,
-    createI18nLoader((ctx) => 'custom_US')
-  );
-  app.register(I18nToken, I18n);
-  // $FlowFixMe
-  app.register(UniversalEventsToken, {
-    from: () => ({emit: () => {}}),
+  test('custom locale resolver', async () => {
+    const app = new App('el', (el) => el);
+    app.register(
+      I18nLoaderToken,
+      createI18nLoader((ctx) => 'custom_US')
+    );
+    app.register(I18nToken, I18n);
+    // $FlowFixMe
+    app.register(UniversalEventsToken, {
+      from: () => ({emit: () => {}}),
+    });
+    app.middleware({i18n: I18nToken}, ({i18n}) => {
+      return (ctx, next) => {
+        const translator = i18n.from(ctx);
+        expect(translator.translate('foo')).toBe('qux');
+        return next();
+      };
+    });
+    const simulator = getSimulator(app);
+    await simulator.render('/', {headers: {'accept-language': 'en_US'}});
   });
-  app.middleware({i18n: I18nToken}, ({i18n}) => {
-    return (ctx, next) => {
-      const translator = i18n.from(ctx);
-      expect(translator.translate('foo')).toBe('qux');
-      return next();
-    };
+
+  afterEach(() => {
+    fs.unlinkSync('translations/en_US.json');
+    fs.unlinkSync('translations/custom_US.json');
+    fs.rmdirSync('translations');
   });
-  const simulator = getSimulator(app);
-  await simulator.render('/', {headers: {'accept-language': 'en_US'}});
-  fs.unlinkSync('translations/en_US.json');
-  fs.unlinkSync('translations/custom_US.json');
-  fs.rmdirSync('translations');
 });
 
 test('no translations dir', () => {
