668ab332b5df5334891bee76f94ffce049260690
Ryan Tsao <rtsao@uber.com>
2022-04-08 19:27:41 +0000

    Improve error message if i18n context not found
    Before this would throw an error about referencing a property `bind` on undefined. Now the error is more useful.

--- fusion-plugin-i18n-react/src/use-translations.js
+++ fusion-plugin-i18n-react/src/use-translations.js
@@ -11,6 +11,10 @@ import {I18nContext} from './plugin.js';
 
 export function useTranslations() {
   const i18n = useContext(I18nContext);
-
+  if (!i18n || !i18n.translate) {
+    throw new Error(
+      'Could not find i18n React context. Perhaps the context provider is missing (i.e. fusion-plugin-i18n-react was not registered) or the provider and useTranslations are referencing different instances of the fusion-plugin-i18n-react React context (i.e. multiple instances of fusion-plugin-i81n-react in the dependency tree).'
+    );
+  }
   return useMemo(() => i18n.translate.bind(i18n), [i18n]);
 }
