cc467daae000b1bd8ce7816e53c5141747a5d4b4
Siarhei Fedarovich <siarhei@uber.com>
2022-07-01 22:40:40 +0000

    Use unique resource name with workerUrl
    Append `?workerUrl=true` resource query to a "virtual" module created using `workerUrl()`. This intends to resolve an issue , where there're duplicate entries being generated in the source maps that reference the same resource path of imported worker script, which can not be represented in devtools accordingly.
    
    ```
    // client-main.js.map
    ...
    "/foo/bar/src/worker.js" ... (Changes to => "/foo/bar/src/worker.js?workerUrl=true")
    "module.exports = __webpack_public_path__ + \"worker.js\";
    
    // worker.js.map
    ...
    "/foo/bar/src/worker.js" ...
    "console.log('test worker');"
    ```

--- fusion-cli/build/babel-plugins/babel-plugin-workerurl/index.js
+++ fusion-cli/build/babel-plugins/babel-plugin-workerurl/index.js
@@ -42,7 +42,9 @@ function refsHandler(t, context, refs = []) {
     }
     args[0].replaceWith(
       t.callExpression(t.identifier('require'), [
-        t.stringLiteral(`__SECRET_WORKER_LOADER__!${args[0].node.value}`),
+        t.stringLiteral(
+          `__SECRET_WORKER_LOADER__!${args[0].node.value}?workerUrl=true`
+        ),
       ])
     );
   });
