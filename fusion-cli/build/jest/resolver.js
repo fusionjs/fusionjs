91783e23bc0e03bb10582a89224830b970959141
Chase <starr@uber.com>
2022-07-15 18:30:56 +0000

    Upgrade enhanced-resolve to enable subpath exports in jest config
    A newer version of `enhanced-resolve` supports [node subpath exports](https://nodejs.org/api/packages.html#subpath-exports) so in this PR I've upgraded to the latest release in fusion-cli. I noticed that the jest resolver is copied into `jest-resolver-fusion` package with a different `enhanced-resolve` version. Should that be updated as well?
    
    Most of this upgrade was straightforward besides handing the `es5-ext` package paths. Enhanced-resolve will escape `#` characters in file paths. I did some digging in webpack and found that they are using the async resolve method that will include the unescaped path, but in the sync variant it will only output escaped. Before the unescaped path was available, webpack would regex replace the `\0#` substrings so that's what I've implemented here.
    
    I tested this change in web-code by resolving `fusion-cli/enhanced-resolve` to `5.10.0` and patching `fusion-cli` with the changes included in this PR.
    
    Phab revision: https://code.uberinternal.com/D7832723
    Passing CI build: https://buildkite.com/uber/web-code/builds/248921

--- fusion-cli/build/jest/resolver.js
+++ fusion-cli/build/jest/resolver.js
@@ -13,6 +13,7 @@ const nodeResolver = ResolverFactory.createResolver({
   useSyncFileSystemCalls: true,
   mainFields: ['main'],
   aliasFields: ['es2015', 'es2017'],
+  conditionNames: ['node', 'require'],
 });
 
 const browserResolver = ResolverFactory.createResolver({
@@ -21,7 +22,30 @@ const browserResolver = ResolverFactory.createResolver({
   useSyncFileSystemCalls: true,
   mainFields: ['main'],
   aliasFields: ['browser', 'es2015', 'es2017'],
+  conditionNames: ['node', 'require'],
 });
+
+// The es5-ext package has files within a '#' directory. Ex. es5-ext/string/#/contains
+//
+// Enhanced-resolve will escape '#' characters by prefixing with a null terminator '\0'
+// https://github.com/webpack/enhanced-resolve/pull/255/files
+//
+// Webpack will then find instances of this escape pattern and convert back to '#'
+// this commit below shows how webpack is transitioning from regex replace to a new
+// enhanced-resolve feature that provides an unescaped path.
+// https://github.com/webpack/webpack/commit/0197867237cd1ef4c1e13d5fcafc71f4585bff9d
+//
+// This feature is only available on the async resolve method on Resolver.
+// https://github.com/webpack/enhanced-resolve/blob/4b16e4c7ee9fc79f6a544a2018baaedb2f7a340f/lib/Resolver.js#L237
+//
+// Because this file uses the resolveSync method, the unescaped path is not available,
+// it will take the webpack's original approach of regex replace.
+function unescapePathFragment(resolverResult) {
+  if (typeof resolverResult === 'string') {
+    return resolverResult.replace(/\0/g, '');
+  }
+  return resolverResult;
+}
 
 module.exports = function enhancedResolve(modulePath, opts) {
   try {
@@ -32,7 +56,7 @@ module.exports = function enhancedResolve(modulePath, opts) {
         modulePath
       );
       if (browserResult !== false) {
-        return browserResult;
+        return unescapePathFragment(browserResult);
       }
       // Fallback to non-browser field if enhanced-resolve produces `false`.
       // Some packages (e.g. object-inspect) use falsy browser field values to
@@ -41,7 +65,8 @@ module.exports = function enhancedResolve(modulePath, opts) {
       // So we simply fall back to resolution without browser field in this case
     }
 
-    return nodeResolver.resolveSync({}, opts.basedir, modulePath);
+    const nodeResult = nodeResolver.resolveSync({}, opts.basedir, modulePath);
+    return unescapePathFragment(nodeResult);
   } catch (err) {
     // Upon failure to resolve, enhanced-resolve throws with a different
     // error message than native Node and no error.code property.
